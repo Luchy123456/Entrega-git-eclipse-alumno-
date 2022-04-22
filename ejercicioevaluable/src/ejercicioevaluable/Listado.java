b14c6970f6659db311526d34c48cd0a977c65def
Luchy <luciaromeraferreiro08@gmail.com>
2022-04-22 13:42:15 +0200

    'inicio de proyecto'

---
+++ ejercicioevaluable/src/ejercicioevaluable/Listado.java
@@ -0,0 +1,12 @@
+package ejercicioevaluable;
+
+public class Listado {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Listado de Alumnos 1º DAW");
+		
+		System.out.println("Alumno 1: ");
+	}
+
+}
